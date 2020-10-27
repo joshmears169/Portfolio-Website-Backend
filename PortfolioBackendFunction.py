8d5430ef0c64f2c564f04b0b991f302a59999712
Josh Mears <joshmears169@hotmail.com>
2020-10-27 12:31:46 +0000

    Test

--- PortfolioBackendFunction.py
+++ PortfolioBackendFunction.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 from decimal import Decimal
-import os
+#import os
 
 ## Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -11,8 +11,8 @@ class DecimalEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj) 
 
 
-DEFAULT_REGION = "eu-west-2"
-region_name = os.environ.get("AWS_REGION", DEFAULT_REGION)
+#DEFAULT_REGION = "eu-west-2"
+#region_name = os.environ.get("AWS_REGION", DEFAULT_REGION)
 
 def lambda_handler(event, context):  
 
